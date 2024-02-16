77f26482df85f084823afc72cecc7eab9e703644
jappatel <pateljap1510@gmail.com>
2024-02-16 15:11:27 +0530

    login updt

--- src/user/Login.js
+++ src/user/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('http://quick-1k8t.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
