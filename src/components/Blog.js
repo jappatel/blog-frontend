cc062b22aea14669bc7c5db8a44df9e9c5ae57bf
jappatel <pateljap1510@gmail.com>
2024-02-19 15:29:12 +0530

    new

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -66,42 +66,38 @@ const Blog = () => {
   return (
     <>
     
-      <div className='flex justify-evenly mt-32'>
-      
-        <div className=''>
-          <h1 className="font-bold text-2xl mb-5 fixed top-20">Table of contents</h1>
-          <ul className='fixed top-36 text-lg font-semibold'>
-            {h1Tags.map((tag, index) => (
-              <li className='hover:text-red-600 hover:text-xl' key={index} onClick={() => handleH1Click(index)} style={{ cursor: 'pointer' }}>{tag.split(' ').slice(0, 3).join(' ')}</li>
-            ))}
-            {h2Tags.map((tag, index) => (
-              <li className='hover:text-red-600 hover:text-xl' key={index} onClick={() => handleH2Click(index)} style={{ cursor: 'pointer' }}>{tag.split(' ').slice(0, 3).join(' ')}</li>
-            ))}
-          </ul>
-         
-        </div>
-         
-
-        <div className='flex flex-col w-1/2'>
-          <div className='justify-between flex'>
-            <div>
-              <p className='text-start ml-5'>{new Date(selectedBlog.createdAt).toLocaleDateString()}</p>
-              <p className='text-start ml-5'>{selectedBlog.author}</p>
-            </div>
-            <div className='flex gap-5 pr-4 text-red-600'>
-              {authorName === selectedBlog.author ? (
-                <h1 className='cursor-pointer'>
-                  <Link key={selectedBlog._id} to={{ pathname: `/editblog/${selectedBlog._id}`, state: { postId: selectedBlog._id } }}>EDIT</Link>
-                </h1>
-              ) : null}
-              <h1 className="cursor-pointer">SHARE</h1>
-            </div>
-          </div>
-          <p className='text-center font-bold text-4xl font-serif pt-5 pb-10'>{selectedBlog.title}</p>
-          <div ref={contentRef} className='text-start select-none text-xl' dangerouslySetInnerHTML={{ __html: selectedBlog.content }} />
-        </div>
+    <div className='flex justify-evenly mt-32 flex-col md:flex-row'>
+  {/* Conditionally render the TOC only on medium-sized screens and above */}
+  <div className='hidden md:block'>
+    <h1 className="font-bold text-2xl mb-5 fixed top-20">Table of contents</h1>
+    <ul className='fixed top-36 text-lg font-semibold'>
+      {h1Tags.map((tag, index) => (
+        <li className='hover:text-red-600 hover:text-xl' key={index} onClick={() => handleH1Click(index)} style={{ cursor: 'pointer' }}>{tag.split(' ').slice(0, 3).join(' ')}</li>
+      ))}
+      {h2Tags.map((tag, index) => (
+        <li className='hover:text-red-600 hover:text-xl' key={index} onClick={() => handleH2Click(index)} style={{ cursor: 'pointer' }}>{tag.split(' ').slice(0, 3).join(' ')}</li>
+      ))}
+    </ul>
+  </div>
+  <div className='flex flex-col w-full md:w-1/2'>
+    <div className='justify-between flex flex-col md:flex-row'>
+      <div>
+        <p className='text-start md:text-center ml-5 md:ml-0'>{new Date(selectedBlog.createdAt).toLocaleDateString()}</p>
+        <p className='text-start md:text-center ml-5 md:ml-0'>{selectedBlog.author}</p>
       </div>
-
+      <div className='flex gap-5 pr-4 text-red-600'>
+        {authorName === selectedBlog.author ? (
+          <h1 className='cursor-pointer'>
+            <Link key={selectedBlog._id} to={{ pathname: `/editblog/${selectedBlog._id}`, state: { postId: selectedBlog._id } }}>EDIT</Link>
+          </h1>
+        ) : null}
+        <h1 className="cursor-pointer">SHARE</h1>
+      </div>
+    </div>
+    <p className='text-center font-bold text-4xl font-serif pt-5 pb-10'>{selectedBlog.title}</p>
+    <div ref={contentRef} className='text-start select-none text-xl' dangerouslySetInnerHTML={{ __html: selectedBlog.content }} />
+  </div>
+</div>
 
       
     </>
