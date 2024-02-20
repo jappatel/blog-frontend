e680700d434a01f23301c67cfcee16c4537ab063
jappatel <pateljap1510@gmail.com>
2024-02-20 10:39:23 +0530

    nav home

--- src/components/Home_Nav.js
+++ src/components/Home_Nav.js
@@ -13,20 +13,16 @@ const Home_Nav = () => {
 
   return (
     <>
-    <div className='divHome  bg-slate-200 px-20 py-20 flex justify-start items-center'>
+    <div className='divHome bg-slate-200 px-10 py-10 md:px-20 md:py-20 flex justify-start items-center'>
 
-    <div className='w-1/2 space-y-4  text-pretty   ' >
+        <div className='w-full md:w-1/2 space-y-4 text-pretty'>
 
-        {/* <h1 className='text-4xl font-bold'>Website evi banavani ,loko ke kasek thi uthayeli che...</h1> */}
-        <h1 className='text-4xl font-bold'>A Blog for Passionate People and Website Lovers</h1>
-        <h2 className='text-xl font-light'>Wealth Creation is an evolutionarily recent positive-sum game. Status is an old zero-sum game. Those attacking wealth creation are often just seeking status.</h2>
-        {/* <button className='relative bg-gradient-to-r from-rose-400 to-fuchsia-400 p-2 rounded-2xl' onClick={handleReadArticleClick}>READ ARTICLE</button> */}
-        
+            <h1 className='text-4xl font-bold'>A Blog for Passionate People and Website Lovers</h1>
+            <h2 className='text-xl font-light'>Wealth Creation is an evolutionarily recent positive-sum game. Status is an old zero-sum game. Those attacking wealth creation are often just seeking status.</h2>
+
+        </div>
     </div>
-</div>
-
-
-    </>
+</>
   )
 }
 
